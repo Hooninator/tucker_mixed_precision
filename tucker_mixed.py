463cd1487d8d416813650bdcd16d7f0f1be476f8
Hooninator <jbellavita@berkeley.edu>
2025-05-23 11:55:00 -0700

    scaled ugemm

--- tucker_mixed.py
+++ tucker_mixed.py
@@ -19,11 +19,25 @@ from scipy.io import loadmat
 class Config:
 
     def __init__(self):
-        return
+        return # Everything blank for now
 
     def set_from_args(self, args):
-        return
+        self.lra_u = args.lra_u
+        self.ttmc_u = args.ttmc_u
+        self.lra_fn = lra_fns[args.lra]
+        self.qr_fn = qr_fns[args.qrd]
+        self.init_fn = lra_fns[args.init]
 
+    def print(self):
+        print("~~~~~~~~~~ CONFIGURATION ~~~~~~~~~~")
+        print(f"    Low-Rank Approximation Precision: {self.lra_u}")
+        print(f"    TTMc Precision: {self.ttmc_u}")
+        print(f"    Factor Matrix Init: {self.init_fn.__name__}")
+        print(f"    Factor Matrix Update: {self.lra_fn.__name__}")
+        print(f"    QR Decomposition: {self.qr_fn.__name__}")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+config = Config()
 
 precisions = {
     "fp16": torch.float16,
@@ -67,10 +81,10 @@ def to_u_gpu(X, u):
     return X.to(device='cuda', dtype=precisions[u])
 
 
-def m_to_u_gpu(tensors, precisions):
+def m_to_u_gpu(tensors, precision):
     result = []
-    for t, p in zip(tensors, precisions):
-        result.append(to_u_gpu(t, p))
+    for t in tensors:
+        result.append(to_u_gpu(t, precision))
     return (*result,)
 
 
@@ -78,15 +92,16 @@ def to_u_cpu(X, u):
     return X.to(device='cpu', dtype=precisions[u])
 
 
-def m_to_u_cpu(tensors, precisions):
+def m_to_u_cpu(tensors, precision):
     result = []
-    for t, p in zip(tensors, precisions):
-        result.append(to_u_cpu(t, p))
+    for t in tensors:
+        result.append(to_u_cpu(t, precision))
     return (*result,)
 
 
 def converged(e1, e2, tol):
     return abs(e1 - e2) < tol
+
 
 def form_core(X, U_list):
     Y = tl.tenalg.multi_mode_dot(X.numpy(), U_list, transpose=True)
@@ -97,6 +112,32 @@ def reconstruct(G, U_list):
     Y = tl.tenalg.multi_mode_dot(G.numpy(), U_list)
     return torch.tensor(Y)
 
+# Take row or columnwise norm of B, then scale appropriate dimension of A
+def nrm_scale(A, B, dim, inv):
+    norms = torch.norm(B, dim=dim)
+    if inv:
+        norms = torch.reciprocal(norms)
+    if dim==0:
+        A_scaled = A / norms[None, :] # Scale each column
+    elif dim==1:
+        A_scaled = A / norms[:, None] # Scale each row
+    else:
+        raise Exception("invalid dimension")
+    return A_scaled
+
+
+def scaled_ugemm(A, B, precision):
+    norms = torch.norm(A, dim=1)
+    for i in range(len(norms)):
+        if norms[i]==0:
+            norms[i] = 1
+    A_scaled = A / norms[:, None]
+    d_A, d_B = m_to_u_gpu([A_scaled, B], precision)
+    d_C_scaled = d_A @ d_B
+    C_scaled = to_u_cpu(d_C_scaled, "fp64")
+    inv_norms = torch.reciprocal(norms)
+    C = C_scaled / inv_norms[:, None]
+    return C
 
 ##################################################
 #
@@ -110,22 +151,23 @@ def hh_qr(X):
 
 
 def chol_qr(X):
-    d_X, d_X_T = m_to_u_gpu([X, X.T], ["fp64", "fp64"])
+    X_T = X.T
+    d_X_T, d_X = m_to_u_gpu([X_T, X], "fp64")
     d_B = torch.matmul(d_X_T, d_X)
     B = to_u_cpu(d_B, "fp64")
     L = torch.linalg.cholesky(B)
-    Q = torch.linalg.solve_triangular(L, X.T, upper=False).T
+    Q = torch.linalg.solve_triangular(L, X_T, upper=False).T
     return Q
 
 
-qr_ops = {
+qr_fns = {
     "hh_qr": hh_qr,
     "chol_qr": chol_qr
 }
 
 
 def qr(args, X):
-    qr_fn = qr_ops[args.qrd]
+    qr_fn = config.qr_fn
     return qr_fn(X)
 
 ##################################################
@@ -135,49 +177,47 @@ def qr(args, X):
 ##################################################
 
 
-def rrf(args, X, r):
+def rrf(X, r):
+    oversampling = 5
     nrm = torch.linalg.norm(X)
-    S = make_gaussian(X.shape[1], r)
-    d_S, d_X = m_to_u_gpu([S,X], ["fp64", "fp64"])
-    Y = torch.matmul(d_X, d_S)
-    Y = to_u_cpu(Y, "fp64")
+    S = make_gaussian(X.shape[1], r+oversampling)
+    Y = scaled_ugemm(X, S, config.lra_u)
     return qr(args, Y)
 
 
-def svd(args, X, r):
+def svd(X, r):
     U, _, _ = torch.linalg.svd(X)
     return U[:, :r]
 
 
-def rand_svd(args, X, r):
-    Q = rrf(args, X, r)
-    B = Q.T @ X
-    U_tilde, _, _ = torch.linalg.svd(B)
-    return Q @ U_tilde[:, :r]
+def rand_svd(X, r):
+    Q = rrf(X, r) 
+    B_T = scaled_ugemm(X.T, Q, config.lra_u).T
+    U_tilde, _, _ = torch.linalg.svd(B_T) # O(R^{2N})
+    return scaled_ugemm(Q, U_tilde[:,:r], config.lra_u)#Q @ U_tilde[:, :r]
 
 
-lra_ops = {
+lra_fns = {
     "rrf": rrf,
     "svd": svd,
     "rand_svd": rand_svd,
 }
 
 
-def init_factors(args, X, ranks):
+def init_factors(X, ranks):
     order = X.ndim
     assert order == len(ranks)
     factors = []
     for k in range(order):
         X_k = unfold(X, k)
-        U_k = lra_ops[args.init](args, X_k, ranks[k])
+        U_k = config.init_fn(X_k, ranks[k])
         factors.append(U_k)
     return factors
 
 
-def update_factor(args, Y, k, rank):
+def update_factor(Y, k, rank):
     Y_k = unfold(Y, k)
-    lra_fn = lra_ops[args.lra]
-    U_k = lra_fn(args, Y_k, rank)
+    U_k = config.lra_fn(Y_k, rank)
     return U_k
 
 ##################################################
@@ -195,14 +235,12 @@ def ttmc(X, matrices, transpose, exclude=[]):
             continue
         Y = torch.tensor(tl.unfold(Y.numpy(), i))
         U = matrices[i]
-        d_Y, d_U = m_to_u_gpu([Y, U], ["fp64", "fp64"])
         if transpose:
-            d_Y = d_U.T @ d_Y
+            Y = scaled_ugemm(Y.T, U, config.ttmc_u).T
             dims[i] = U.shape[1]
         else:
-            d_Y = d_U @ d_Y
+            Y = scaled_ugemm(U, Y, config.ttmc_u)
             dims[i] = U.shape[0]
-        Y = to_u_cpu(d_Y, "fp64")
         Y = torch.tensor(tl.fold(Y.numpy(), i, dims))
     return Y
 
@@ -216,7 +254,7 @@ def ttmc(X, matrices, transpose, exclude=[]):
 def hooi(args, X, ranks):
 
     # Initialize factor matrices with RRF
-    U_list = init_factors(args, X, ranks)
+    U_list = init_factors(X, ranks)
     G = form_core(X, U_list)
     err_curr = compute_error(X, G, U_list)
     err_prev = torch.linalg.norm(X)
@@ -230,7 +268,7 @@ def hooi(args, X, ranks):
 
         for n in range(N):
             Y = ttmc(X, U_list, True, [n])
-            U_n = update_factor(args, Y, n, ranks[n])
+            U_n = update_factor(Y, n, ranks[n])
             U_list[n] = U_n
 
         G = form_core(X, U_list)
@@ -254,9 +292,6 @@ def write_stats(args, error_mat):
     return
 
 
-def print_args(args):
-    return
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--tensorpath", type=str)
@@ -267,12 +302,12 @@ if __name__ == "__main__":
     parser.add_argument("--lra", type=str)
     parser.add_argument("--qrd", type=str)
     parser.add_argument("--init", type=str)
-    parser.add_argument("--init_u", type=str)
     parser.add_argument("--ttmc_u", type=str)
     parser.add_argument("--lra_u", type=str)
     parser.add_argument("--ranks", type=int, nargs='+')
     args = parser.parse_args()
-    print_args(args)
+    config.set_from_args(args)
+    config.print()
 
     X = read_tensor(args)
     print(X.shape)
