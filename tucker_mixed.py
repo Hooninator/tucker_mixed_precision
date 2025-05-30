first pass better scaling + less overhead -- runs but incorrect

tucker_mixed.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorly as tl
@@ -31,11 +30,17 @@ class Config:
     def set_from_args(self, args):
         self.lra_u = args.lra_u
         self.ttmc_u = args.ttmc_u
-        self.lra_fn = lra_fns[args.lra]
-        self.qr_fn = qr_fns[args.qrd]
-        self.init_fn = lra_fns[args.init]
+        if args.mode == "cuda":
+            self.lra_fn = gpu_lra_fns[args.lra]
+            self.qr_fn = gpu_qr_fns[args.qrd]
+            self.init_fn = gpu_lra_fns[args.init]
+        else:
+            self.lra_fn = lra_fns[args.lra]
+            self.qr_fn = qr_fns[args.qrd]
+            self.init_fn = lra_fns[args.init]
         self.ranks = args.ranks
         self.tensorname = get_tensorname(args.tensorpath)
+        self.mode = args.mode
 
     def print(self):
         print("~~~~~~~~~~ CONFIGURATION ~~~~~~~~~~")
@@ -85,13 +90,54 @@ def compute_error(X, G, U_list):
     return (torch.linalg.norm(X - T) / torch.linalg.norm(X)).item()
 
 
+def gpu_compute_error(d_X, d_G, d_U_list, d_D_list):
+    # Unscale each U
+    N = d_X.ndim
+    for n in range(N):
+        d_U_list[n] = d_U_list[n] * d_D_list[n][:, None]
+
+    d_G = to_u(d_G, 'fp64')
+    d_T = ttmc_fast(d_G, d_U_list, False)
+
+    # Rescale
+    for n in range(N):
+        d_U_list[n] = d_U_list[n] / d_D_list[n][:, None]
+        d_U_list[n] = to_u(d_U_list[n], config.ttmc_u)
+
+    d_X = to_u(d_X, 'fp64')
+    return (torch.linalg.norm(d_X - d_T) / torch.linalg.norm(d_X)).item()
+
+
 def unfold(X, mode):
+    if config.mode=="cuda":
+        return gpu_unfold(X, mode)
+    else:
+        return cpu_unfold(X, mode)
+
+
+def cpu_unfold(X, mode):
     X_k = tl.base.unfold(X.numpy(), mode)
     return torch.tensor(X_k)
 
 
-def make_gaussian(m, n):
-    return torch.randn(m, n, dtype=torch.float64)
+def gpu_unfold(d_X, mode):
+    # Slow
+    h_X = d_X.to(device='cpu')
+    return cpu_unfold(h_X, mode).to(device='cuda')
+
+
+def gpu_fold(d_X, mode, dims):
+    h_X = d_X.to(device='cpu')
+    h_X = torch.tensor(tl.fold(h_X.numpy(), mode, dims))
+    return h_X.to(device='cuda')
+
+
+def make_gaussian(m, n, device='cpu', precision='fp64'):
+    return torch.randn(m, n, dtype=precisions[u], device=device)
+
+
+def to_u(X, u):
+    return X.to(dtype=precisions[u])
 
 
 def to_u_gpu(X, u):
@@ -116,6 +162,10 @@ def m_to_u_cpu(tensors, precision):
     return (*result,)
 
 
+def alloc_gpu_tensor(dims, u):
+    return torch.zeros(dims, dtype=precisions[u], device='cuda')
+
+
 def converged(e1, e2, tol):
     return abs(e1 - e2) < tol
 
@@ -125,15 +175,35 @@ def form_core(X, U_list):
     return torch.tensor(Y)
 
 
+def gpu_form_core(d_X, d_U_list, d_D_list):
+    # Unscale each U
+    N = d_X.ndim
+    for n in range(N):
+        d_U_list[n] = d_U_list[n] * d_D_list[n][:, None] # Converts to fp64
+
+    # form the core as normal
+    d_X = to_u(d_X, 'fp64')
+    d_G = ttmc_fast(d_X, d_U_list, True)
+
+    # Rescale
+    for n in range(N):
+        d_U_list[n] = d_U_list[n] / d_D_list[n][:, None]
+        d_U_list[n] = to_u(d_U_list[n], config.ttmc_u)
+
+    return d_G
+
+
+
 def reconstruct(G, U_list):
     Y = tl.tenalg.multi_mode_dot(G.numpy(), U_list)
     return torch.tensor(Y)
 
-# Take row or columnwise norm of B, then scale appropriate dimension of A
-
 
 def nrm_scale(A, B, dim, inv):
     norms = torch.norm(B, dim=dim)
+    for i in range(len(norms)):
+        if norms[i] == 0:
+            norms[i] = 1
     if inv:
         norms = torch.reciprocal(norms)
     if dim == 0:
@@ -145,6 +215,21 @@ def nrm_scale(A, B, dim, inv):
     return A_scaled
 
 
+def compute_scaling_row(d_X):
+    norms = torch.norm(d_X, dim=1)
+    for i in range(len(norms)):
+        if norms[i] == 0:
+            norms[i] = 1
+    return norms
+
+
+# TODO: row and column scaling -- iterative row and column scaling
+# can write a least squares problem to get the best possible diagonal entries
+# scales rows and columns iteratively -- one/two times 
+# Evertything in GPU --- scaling may not be needed 
+# Persistenyl store input tensor and factors in fp16 -- dont' convert a bunch
+# and also apply some noramlzjation thing at the start to the tensor once 
+# keep accumulating lambada scaling factor -- this can be in high precision -- then scale back once at the very end
 def scaled_ugemm(A, B, precision):
     norms = torch.norm(A, dim=1)
     for i in range(len(norms)):
@@ -187,14 +272,32 @@ def chol_qr(X):
     return Q
 
 
+def gpu_chol_qr(d_X):
+    d_B = d_X.T @ d_X
+    L = torch.linalg.cholesky(B)
+    Q = torch.linalg.solve_triangular(L, X_T, upper=False).T
+    return Q
+
+
 qr_fns = {
     "hh_qr": hh_qr,
     "chol_qr": chol_qr
 }
 
 
-def qr(args, X):
+gpu_qr_fns = {
+    "hh_qr": hh_qr,
+    "chol_qr": gpu_chol_qr
+}
+
+
+def qr(X):
     qr_fn = config.qr_fn
+    return qr_fn(X)
+
+
+def gpu_qr(X):
+    qr_fn = config.gpu_qr_fns
     return qr_fn(X)
 
 ##################################################
@@ -205,16 +308,25 @@ def qr(args, X):
 
 
 def rrf(X, r):
-    oversampling = 5
+    l = 5
     nrm = torch.linalg.norm(X)
-    S = make_gaussian(X.shape[1], r+oversampling)
+    S = make_gaussian(X.shape[1], r+l) 
     Y = scaled_ugemm(X, S, config.lra_u)
-    return qr(args, Y)
+    return qr(Y)
+
+
+def gpu_rrf(d_X, r):
+    l = 5
+    S = make_gaussian(d_X.shape[1], r+l, 'cuda', config.ttmc_u) #TODO: Only do this once?
+    Y = d_X @ S
+    Y = to_u(Y, 'fp64')
+    return gpu_qr(Y)
 
 
 def svd(X, r):
-    U, _, _ = torch.linalg.svd(X)
-    return U[:, :r]
+    U, _, _ = torch.linalg.svd(to_u(X, config.lra_u))
+    U_r = to_u(U[:, :r], config.ttmc_u)
+    return U_r
 
 
 def rand_svd(X, r):
@@ -224,10 +336,27 @@ def rand_svd(X, r):
     return scaled_ugemm(Q, U_tilde[:, :r], config.lra_u)  # Q @ U_tilde[:, :r]
 
 
+def gpu_rand_svd(d_X, r):
+    Q = gpu_rrf(d_X, r)
+    Q = to_u(Q, config.ttmc_u)
+    B = Q.T @ d_X
+    B = to_u(B, config.lra_u)
+    d_U_tilde, _, _, = torch.linalg.svd(B)
+    d_U_tilde = to_u(d_U_tilde, config.ttmc_u)
+    return Q @ d_U_tilde[:, :r]
+
+
 lra_fns = {
     "rrf": rrf,
     "svd": svd,
     "rand_svd": rand_svd,
+}
+
+
+gpu_lra_fns = {
+    "rrf": gpu_rrf,
+    "svd": svd,
+    "rand_svd": gpu_rand_svd,
 }
 
 
@@ -249,7 +378,41 @@ def update_factor(Y, k, rank):
 
 ##################################################
 #
-#               TTMC FUNCTION
+#               SCALING MATRICES
+#
+##################################################
+
+def init_scaling_matrices(d_X):
+    d_D_list = []
+    N = d_X.ndim
+    dim_lst = np.arange(0, N)
+    for n in range(N):
+        # norm of each n-slice
+        I = d_X.shape[n]
+        d_D = torch.sum(d_X, dim=([d for d in dim_lst if d != n]))
+        d_D_list.append(d_D)
+    return d_D_list
+
+
+def apply_scaling_matrices(d_X, d_D_list, inv):
+    N = d_X.ndim
+    assert N == len(d_D_list)
+    shape = [1] * N
+    for n in range(N):
+        # Scale n-slices 
+        shape[n] = -1
+        if inv:
+            d_X = d_X / d_D_list[n].view(*shape)
+        else:
+            d_X = d_X * d_D_list[n].view(*shape)
+        shape[n] = 1
+    return d_X
+        
+
+
+##################################################
+#
+#               TTMC FUNCTIONS
 #
 ##################################################
 
@@ -272,6 +435,28 @@ def ttmc(X, matrices, transpose, exclude=[]):
         Y = torch.tensor(tl.fold(Y.numpy(), i, dims))
     return Y
 
+
+def ttmc_fast(d_X, matrices, transpose, exclude=[]):
+    n = len(matrices)
+    dims = list(d_X.shape)
+    d_Y = d_X
+    for i in range(n):
+        if i in exclude:
+            continue
+        d_Y = gpu_unfold(d_Y, i)
+        d_U = matrices[i]
+        if transpose:
+            d_Y = d_U.T @ d_Y
+            #d_Y = scaled_ugemm(d_Y.T, d_U, config.ttmc_u).T
+            dims[i] = d_U.shape[1]
+        else:
+            d_Y = d_U @ d_Y
+            #d_Y = scaled_ugemm(d_U, d_Y, config.ttmc_u)
+            dims[i] = d_U.shape[0]
+        #d_Y = torch.tensor(tl.fold(Y.numpy(), i, dims))
+        d_Y = gpu_fold(d_Y, i, dims)
+    return d_Y
+
 ##################################################
 #
 #               MAIN HOOI FUNCTION
@@ -279,7 +464,7 @@ def ttmc(X, matrices, transpose, exclude=[]):
 ##################################################
 
 
-def hooi(args, X, ranks):
+def hooi(X, ranks, maxiters):
 
     # Initialize factor matrices
     U_list = init_factors(X, ranks)
@@ -289,7 +474,6 @@ def hooi(args, X, ranks):
     errors = [err_curr]
 
     # Main loop
-    maxiters = args.maxiters
     iter = 0
     N = X.ndim
     while iter < maxiters:
@@ -308,6 +492,51 @@ def hooi(args, X, ranks):
 
     print(f"Final error: {err_curr}")
     return errors
+
+
+def hooi_fast(X, ranks, maxiters):
+
+    # Move tensor to GPU 
+    d_X = to_u_gpu(X, "fp64") # Keep things in fp64 for now, since we need to do scaling
+    d_X_unscaled = to_u_gpu(X, "fp64") # Unscaled copy for error computation
+
+    # Compute scaling matrices
+    d_D_list = init_scaling_matrices(d_X)
+    d_X = apply_scaling_matrices(d_X, d_D_list, True)
+
+    # Convert to ttmc precision
+    d_X = to_u(d_X, config.ttmc_u)
+
+    # Init factor matrices
+    d_U_list = init_factors(d_X, ranks)
+
+    d_G = gpu_form_core(d_X, d_U_list, d_D_list) # G = unscaled factors * scaled X
+    err_curr = gpu_compute_error(d_X, d_G, d_U_list, d_D_list)
+    err_prev = torch.linalg.norm(d_X).to(device='cpu')
+    errors = [err_curr]
+
+    # Main loop
+    iter = 0
+    N = d_X.ndim
+    while iter < maxiters:
+
+        for n in range(N):
+            d_Y_n = ttmc_fast(d_X, d_U_list, True, [n])
+            d_U_n = update_factor(d_Y_n, n, ranks[n])
+            d_U_list[n] = d_U_n
+        
+        d_G = gpu_form_core(d_X, d_U_list, d_D_list)
+        err_prev = err_curr
+        err_curr = gpu_compute_error(d_X, d_G, d_U_list, d_D_list)
+
+        iter += 1
+        errors.append(err_curr)
+
+    print(f"Final error: {err_curr}")
+    return errors
+
+
+
 
 ##################################################
 #
@@ -340,14 +569,15 @@ if __name__ == "__main__":
     parser.add_argument("--init", type=str)
     parser.add_argument("--ttmc_u", type=str)
     parser.add_argument("--lra_u", type=str)
+    parser.add_argument("--mode", type=str)
     parser.add_argument("--ranks", type=int, nargs='+')
     args = parser.parse_args()
-    config.set_from_args(args)
-    config.print()
 
     X = read_tensor(args)
-    print(X.shape)
     ranks = args.ranks
+
+    config.set_from_args(args)
+    config.print()
 
     # Pytorch setup
     torch.backends.cuda.matmul.allow_tf32 = True
@@ -364,7 +594,8 @@ if __name__ == "__main__":
     error_mat = np.zeros(shape=(args.ntrials, args.maxiters + 1))
     for i in range(args.ntrials):
         stime = time.time()
-        errors = hooi(args, X, ranks)
+        errors = hooi_fast(X, ranks, args.maxiters)
+        #errors = hooi(X, ranks, args.maxiters)
         etime = time.time()
         print(f"Time: {etime-stime}s")
         error_mat[i] = errors